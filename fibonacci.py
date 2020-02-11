45424b6dc0186a210bb4baeea638682cf8b25cc0
Anorax <anorax.isa@gmail.com>
2020-02-11 20:25:30 +0100

    Just for practice

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,22 @@
-def fib(n):
+"""Just for practice"""
+
+
+def fib(start=0):
+    """
+    Fibonacci function
+    :param start: int
+    :return: int
+    """
     base = [0, 1]
-    a = base[0]
-    b = base[1]
-    for i in range(n):
-        base.append(a+b)
-        a = base[i+1]
-        b = base[i+2]
-    return base[n]
+    base_a = base[0]
+    base_b = base[1]
+    for i in range(start):
+        base.append(base_a+base_b)
+        base_a = base[i+1]
+        base_b = base[i+2]
+    return base[start]
+
 
 assert fib(2) == 1
 assert fib(5) == 5
-assert  fib(8) == 21
+assert fib(8) == 21
