5d37231052a2dc8d28e126c14d7cc08e20ec5086
Anorax <anorax.isa@gmail.com>
2020-02-28 09:38:21 +0100

    Refactoring

--- tnef_data_extract.py
+++ tnef_data_extract.py
@@ -1,3 +1,6 @@
+"""
+Extracts PDF file form DAT files and saves them
+"""
 from os import listdir, getcwd
 from os.path import isfile, join
 import hashlib
@@ -8,19 +11,23 @@ from tnefparse.tnef import TNEF
 
 
 def get_hash():
-    h = hashlib.sha256()
-    h.update(bytes(str(time()), encoding='utf-8'))
-    return h.hexdigest()[:20]
+    """
+    Hashing method for filename uniques
+    :return: str
+    """
+    hashed_filename = hashlib.sha256()
+    hashed_filename.update(bytes(str(time()), encoding='utf-8'))
+    return hashed_filename.hexdigest()[:20]
 
 
-all_files_list = [f for f in listdir(getcwd()) if isfile(join(getcwd(), f))]
-dat_files_list = [f for f in all_files_list if '.dat' in f]
+ALL_FILES_LIST = [f for f in listdir(getcwd()) if isfile(join(getcwd(), f))]
+DAT_FILES_LIST = [f for f in ALL_FILES_LIST if '.dat' in f]
 
-processed_files = []
-ommited = []
+PROCESSED_FILES = []
+OMITTED_FILES = []
 
-bar = ShadyBar('Processing', max=len(dat_files_list))
-for file in dat_files_list:
+bar = ShadyBar('Processing', max=len(DAT_FILES_LIST))
+for file in DAT_FILES_LIST:
     t = TNEF(open(file, 'rb').read(), do_checksum=True)
     for i, a in enumerate(t.attachments):
         if '.pdf' in a.name.decode(encoding='utf-8'):
@@ -29,15 +36,15 @@ for file in dat_files_list:
                 name = name.replace('.pdf', f'__{str(get_hash())}.pdf')
                 with open(name, "wb") as afp:
                     afp.write(a.data)
-                    processed_files.append(name)
+                    PROCESSED_FILES.append(name)
             else:
                 with open(a.name, "wb") as afp:
                     afp.write(a.data)
-                    processed_files.append(a.name.decode(encoding='utf-8'))
+                    PROCESSED_FILES.append(a.name.decode(encoding='utf-8'))
         else:
-            ommited.append(a.name.decode(encoding='utf-8'))
+            OMITTED_FILES.append(a.name.decode(encoding='utf-8'))
     bar.next()
 bar.finish()
 
-for item in ommited:
+for item in OMITTED_FILES:
     print(f'Ommited: {item}')
